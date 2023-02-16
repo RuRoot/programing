bc64a4f537cdfd6dd3055029093bbcda17256dfd
RuRoot <122613708+RuRoot@users.noreply.github.com>
2023-02-16 19:11:08 +0300

    Update sorts.h

--- sorts.h
+++ sorts.h
@@ -4,18 +4,18 @@ using namespace std;
 #ifndef SORTS_H
 #define SORTS_H
 
-void mergeSort(int* ar, int size);
-void insertionSort(int* ar,int  size);
-void bubbleSort(int* ar,int size);
+void mergeSort(int* ar, int size);//1
+void insertionSort(int* ar,int  size);//2
+void bubbleSort(int* ar,int size);//3
 void  print (int* ar,int size);
-void selectionSort(int* ar, int size);
+void selectionSort(int* ar, int size);//4
 int retMax(int* ar,int size);
 int retMin(int* ar,int size);
-void countsort(int *ar, int size);
+void countsort(int *ar, int size);//5
 void randomFilling(int* ar,int size,int min,int max);
-     
+void swap(int& a, int& b);     
 
-
+void quickSort(int* ar, int size);//6
 
 
 
