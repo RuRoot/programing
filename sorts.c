4e25d47f8d97ffae55807e7bb7f0a2890ececf3e
RuRoot <122613708+RuRoot@users.noreply.github.com>
2023-02-16 18:47:16 +0300

    Update sorts.c

--- sorts.c
+++ sorts.c
@@ -34,6 +34,47 @@ void insertionSort(int* ar,int size){
 }
 
 
+static void merge(int* ar, int size, int central) {
+    int left = 0;
+    int right = central;
+    int* arTemp = new int [size];
+    int indexTemp = 0;
+
+    while (left < central && right < size) {
+        while(ar[left] <= ar[right] && left < central) {
+            arTemp[indexTemp++] = ar[left++];
+//            left++;
+//            indexTemp++;
+        }
+        while(ar[left] > ar[right] && right < size) {
+            arTemp[indexTemp] = ar[right];
+            indexTemp++;
+            right++;
+        }
+    }
+
+    while (left < central) {
+        arTemp[indexTemp++] = ar[left++];
+    }
+    while (right < size) {
+        arTemp[indexTemp++] = ar[right++];
+    }
+
+    memcpy(ar, arTemp, size * sizeof(int));
+
+    delete [] arTemp;
+}
+
+void mergeSort(int* ar, int size) {
+    if (size <= 1) {
+        return;
+    }
+    mergeSort(&ar[0], size >> 1);
+    mergeSort(&ar[size >> 1], size - (size >> 1));
+
+    merge(ar, size, size >> 1);
+}
+
 void bubbleSort(int* ar,int size){
     int num;
     for(int s=size;s>0;s--){
