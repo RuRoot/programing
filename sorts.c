5fff0b1a2d58434eaf8a7aecf22f1ac0915f3bdd
RuRoot <122613708+RuRoot@users.noreply.github.com>
2023-02-16 19:58:18 +0300

    Update sorts.c

--- sorts.c
+++ sorts.c
@@ -32,6 +32,7 @@ void insertionSort(int* ar,int size){
                 ar[indexArr] = element;//в ручную бахнули элемент с конца по начало 
         }
 }
+
 void swap(int& a, int& b) {
 	int temp = a;
 	a = b;
@@ -96,7 +97,27 @@ static void merge(int* ar, int size, int central) {
 
     delete [] arTemp;
 }
-
+int isSort (int* ar,int size){
+    int key=-1;
+    int i=0;
+   while(i<size-1){
+      if ((ar[i]>ar[i+1] and  key==1) or (ar[i]<ar[i+1] and key==0)){
+          key=-1;
+          
+          break;
+       }
+      else if (ar[i]<=ar[i+1]){
+           key=1;
+           
+       }
+       else if(ar[i]>=ar[i+1]){
+           key=0;
+           
+       }
+       i++;
+   }
+   return key;
+}
 void mergeSort(int* ar, int size) {
     if (size <= 1) {
         return;
